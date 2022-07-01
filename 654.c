9050a72784fd5f3adb8fdf817725d15f2545bf7c
CS65-654234025 <108518363+CS65-654234025@users.noreply.github.com>
2022-07-01 16:08:42 +0700

    Add files via upload

---
+++ 654.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printfl("i am thiland !!");
+        return 0;
+}
+int main(int argc, char const *argv[])
+{
+    printfl("i am thiland !!");
+    return 0;
+}
